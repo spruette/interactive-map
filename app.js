2035671f2e9dff4e6b58a622ce8e628abb8978bb
unknown <scotteychaos@gmail.com>
2022-07-14 18:21:40 -0400

    index file done notes set up for js

---
+++ app.js
@@ -0,0 +1,12 @@
+// map object
+
+// get coordinates via geolocation api
+
+// get foursquare businesses
+
+// process foursquare array
+
+// event handlers
+// window load
+
+// business submit button
